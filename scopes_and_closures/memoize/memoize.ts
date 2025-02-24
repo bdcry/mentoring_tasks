7787e020557d1913a7151f3060acf7275a1e9843
Tim <tim@MacBook-Pro-2.local>
2025-02-24 22:21:56 +0300

    successful solution to the useMemo task

--- scopes_and_closures/memoize/memoize.ts
+++ scopes_and_closures/memoize/memoize.ts
@@ -1,5 +1,18 @@
 function memoize<T extends (...args: any[]) => any>(func: T) {
+  const cashe = new Map();
 
+  // @ts-ignore
+  return function(...args) {
+    const key = args.toString();
+
+    if (cashe.has(key)) {
+      return cashe.get(key);
+    }
+
+    const result = func(...args);
+    cashe.set(key, result);
+    return result;
+  }
 }
 
 export { memoize };
