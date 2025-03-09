0e9a9a98c56f2e7eff041586f94e6244f53bf5c3
Tim <tim@MacBook-Pro-2.local>
2025-03-09 22:47:02 +0300

    needs some work, fails 3 tests

--- algos_and_structures/path.ts
+++ algos_and_structures/path.ts
@@ -1,3 +1,29 @@
-export const path = (obj, path, defaultValue) => {
-    
-}
+  const parsePath = (path: string | Array<string | number>): Array<string | number> => {
+    if (Array.isArray(path)) return path;
+
+    return path
+      .replace(/\[/g, '.')
+      .replace(/\]/g, '')
+      .split('.')
+      .filter((key: string) => key !== '');
+  };
+
+  export const path = <T extends Record<string | number, any>>(obj: T, path: string | Array<string | number>, defaultValue: T): T | undefined => {
+    if (path.length === 0) {
+      return obj;
+    }
+
+    if (!obj) {
+      return defaultValue;
+    }
+
+    const keys = parsePath(path);
+
+    let result = obj;
+
+    for (const key of keys) {
+      if (result === undefined) return defaultValue;
+      result = result[key];
+    }
+    return result === undefined ? defaultValue : result;
+  };
